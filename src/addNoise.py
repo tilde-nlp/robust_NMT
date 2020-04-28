99dacfecc14e7cc239a8f2513e715252dae9b87f
arturs.stafanovics <arturs.stafanovics@tilde.lv>
2020-04-28 15:06:46 +0300

    automatic cleanup

--- src/addNoise.py
+++ src/addNoise.py
@@ -3,46 +3,50 @@ import sys
 import string
 import spell
 
+
 def deleteLetters(word):
-  index = random.randint(1,len(word)-1)
-  while not word[index].isalpha():
-      index = random.randint(1,len(word)-1)
-  return word[0 : index ] + word[index + 1 : ]
+    index = random.randint(1, len(word) - 1)
+    while not word[index].isalpha():
+        index = random.randint(1, len(word) - 1)
+    return word[0: index] + word[index + 1:]
+
 
 def permuteLetters(word):
-  START_INDEX = 1
-  if word[0].isupper():
-    START_INDEX = 2
+    START_INDEX = 1
+    if word[0].isupper():
+        START_INDEX = 2
 
-  if START_INDEX > len(word): # or not checkAplicability(word[START_INDEX:]):
-    return word
-  index = random.randint(START_INDEX,len(word)-1)
-  while not (word[index].isalpha() and word[index-1].isalpha()):
-    index = random.randint(START_INDEX,len(word)-1)
-  return word[0 : index-1] + word[index] + word[index-1] + word[index + 1 : ]
+    if START_INDEX > len(word):  # or not checkAplicability(word[START_INDEX:]):
+        return word
+    index = random.randint(START_INDEX, len(word) - 1)
+    while not (word[index].isalpha() and word[index - 1].isalpha()):
+        index = random.randint(START_INDEX, len(word) - 1)
+    return word[0: index - 1] + word[index] + word[index - 1] + word[index + 1:]
+
 
 def introduceExtraLetters(word):
-  index = random.randint(0,len(word)-1)
-  while not word[index].isalpha():
-      index = random.randint(0,len(word)-1)
-  if (random.random() < 0.5): # double the letter
-    return word[0 : index] + word[index] + word[index] + word[index + 1 : ]
-  else: # adds a nearby letter
-    addition = random.choice(neighbourKeys(word[index]))
-    if word[index].islower():
-      return word[0 : index+1] + addition + word[index + 1 : ]
-    else:
-      return word[0 : index+1] + addition.upper() + word[index + 1 : ]
+    index = random.randint(0, len(word) - 1)
+    while not word[index].isalpha():
+        index = random.randint(0, len(word) - 1)
+    if (random.random() < 0.5):  # double the letter
+        return word[0: index] + word[index] + word[index] + word[index + 1:]
+    else:  # adds a nearby letter
+        addition = random.choice(neighbourKeys(word[index]))
+        if word[index].islower():
+            return word[0: index + 1] + addition + word[index + 1:]
+        else:
+            return word[0: index + 1] + addition.upper() + word[index + 1:]
+
 
 def confuseLetters(word):
-  index = random.randint(0,len(word)-1)
-  while not word[index].isalpha():
-      index = random.randint(0,len(word)-1)
-  confussion = random.choice(neighbourKeys(word[index]))
-  if word[index].islower():
-    return word[0 : index] + confussion + word[index + 1 : ]
-  else:
-    return word[0 : index] + confussion.upper() + word[index + 1 : ]
+    index = random.randint(0, len(word) - 1)
+    while not word[index].isalpha():
+        index = random.randint(0, len(word) - 1)
+    confussion = random.choice(neighbourKeys(word[index]))
+    if word[index].islower():
+        return word[0: index] + confussion + word[index + 1:]
+    else:
+        return word[0: index] + confussion.upper() + word[index + 1:]
 
 
 class StrictList(list):
@@ -61,68 +65,84 @@ class StrictList(list):
             return
         return list.append(self, n)
 
-LV_MAPPING = dict(zip("āčēģīķļņšūž"+"āčēģīķļņšūž".upper(), "acegiklnsuz"+"acegiklnsuz".upper()))
-def latinize(word, mapping=LV_MAPPING):
-  word = "".join([mapping[l] if l in mapping else l for l in word])
-  return word
 
-LV_PHONETIC_MAPPING = dict(zip([l for l in "āčēģīķļņšūž"+"āčēģīķļņšūž".upper()], ["aa", "ch", "ee", "gj", "ii", "kj","lj", "nj", "sh", "uu","zh"]+ [r.capitalize() for r in ["aa", "ch", "ee", "gj", "ii", "kj","lj", "nj", "sh", "uu","zh"]]))
+LV_MAPPING = dict(zip("āčēģīķļņšūž" + "āčēģīķļņšūž".upper(), "acegiklnsuz" + "acegiklnsuz".upper()))
+
+
+def latinize(word, mapping=LV_MAPPING):
+    word = "".join([mapping[l] if l in mapping else l for l in word])
+    return word
+
+
+LV_PHONETIC_MAPPING = dict(zip([l for l in "āčēģīķļņšūž" + "āčēģīķļņšūž".upper()],
+                               ["aa", "ch", "ee", "gj", "ii", "kj", "lj", "nj", "sh", "uu", "zh"] + [r.capitalize() for
+                                                                                                     r in
+                                                                                                     ["aa", "ch", "ee",
+                                                                                                      "gj", "ii", "kj",
+                                                                                                      "lj", "nj", "sh",
+                                                                                                      "uu", "zh"]]))
+
 
 def phoneticLatinize(word, mapping=LV_PHONETIC_MAPPING):
-  return latinize(word, LV_PHONETIC_MAPPING)
+    return latinize(word, LV_PHONETIC_MAPPING)
 
-LV_REVERSE_MAPPING = dict(zip("acegiklnsuz"+"acegiklnsuz".upper(),"āčēģīķļņšūž"+"āčēģīķļņšūž".upper()))
 
-def addDiacritic(word,mapping=LV_REVERSE_MAPPING):
-  indexes = [i for i, l in enumerate(word) if l in mapping]
-  if len(indexes) > 0:
-    index = random.choice(indexes)
-    return word[0 : index] + mapping[word[index]] + word[index + 1 : ]
-  return word
+LV_REVERSE_MAPPING = dict(zip("acegiklnsuz" + "acegiklnsuz".upper(), "āčēģīķļņšūž" + "āčēģīķļņšūž".upper()))
+
+
+def addDiacritic(word, mapping=LV_REVERSE_MAPPING):
+    indexes = [i for i, l in enumerate(word) if l in mapping]
+    if len(indexes) > 0:
+        index = random.choice(indexes)
+        return word[0: index] + mapping[word[index]] + word[index + 1:]
+    return word
 
 
 def removePunctuation(sentence):
     return " ".join([item for item in sentence.split() if item not in string.punctuation])
 
-def neighbourKeys(k): 
-  validChars = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
-              'p','q','r','s','t','u','v','w','x','y','z'}
-  keyboard = StrictList([
-                   StrictList(['q','w','e','r','t','y','u','i','o','p']),
-                   StrictList([None,'a','s','d','f','g','h','j','k','l',';',]),
-                   StrictList([None,'z','x','c','v','b','n','m',',','.',]),
-                   ]) 
-  def check(c):
-    """Return only valid characters."""
-    return c if c in validChars else ''
-   
-  """Return a list of keys physically near character 'k' on the keyboard."""
-  k = latinize(k.lower())
-  keys = StrictList()
 
-  index = []
+def neighbourKeys(k):
+    validChars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+    keyboard = StrictList([
+        StrictList(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']),
+        StrictList([None, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', ]),
+        StrictList([None, 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ]),
+    ])
 
-  for i,row in enumerate(keyboard):
-    if k in row:
-      index.append(i)
-      # Detect whether to go one column forward or back,
-      # based on which row of the keyboard this button is in.
-      offset = 1 if i % 2 == 0 else -1
-      index.append(row.index(k))
-      break
-  else:
-    #print ("Invalid Key: {}".format(k))
-    return [k]
+    def check(c):
+        """Return only valid characters."""
+        return c if c in validChars else ''
 
-  # Iterate over the key's row, the row above and the one below,
-  # pass an IndexError if the referenced index is non existent.
-  for val in ([0,-1], [0, 1],[-1, 0], [-1, offset], [+1, 0], [ 1, offset],):
-    try:
-      neighbourKey = keyboard[index[0] + val[0]][index[1] + val[1]]
-      keys.append(check(neighbourKey))
-    except IndexError:
-      pass
-  return keys
+    """Return a list of keys physically near character 'k' on the keyboard."""
+    k = latinize(k.lower())
+    keys = StrictList()
+
+    index = []
+
+    for i, row in enumerate(keyboard):
+        if k in row:
+            index.append(i)
+            # Detect whether to go one column forward or back,
+            # based on which row of the keyboard this button is in.
+            offset = 1 if i % 2 == 0 else -1
+            index.append(row.index(k))
+            break
+    else:
+        # print ("Invalid Key: {}".format(k))
+        return [k]
+
+    # Iterate over the key's row, the row above and the one below,
+    # pass an IndexError if the referenced index is non existent.
+    for val in ([0, -1], [0, 1], [-1, 0], [-1, offset], [+1, 0], [1, offset],):
+        try:
+            neighbourKey = keyboard[index[0] + val[0]][index[1] + val[1]]
+            keys.append(check(neighbourKey))
+        except IndexError:
+            pass
+    return keys
+
 
 """def checkAplicability(sentence):
   try:
@@ -131,29 +151,30 @@ def neighbourKeys(k):
     return False
 """
 
-noiseFunctions = [(latinize,"<0>") , (phoneticLatinize,"<1>"), (addDiacritic,"<2>"), (deleteLetters,"<3>"), (permuteLetters,"<4>"), (introduceExtraLetters,"<5>"), (confuseLetters,"<6>"), (spell.sampleSubstitute, "<7>"), (removePunctuation, "<8>")]
-
+noiseFunctions = [(latinize, "<0>"), (phoneticLatinize, "<1>"), (addDiacritic, "<2>"), (deleteLetters, "<3>"),
+                  (permuteLetters, "<4>"), (introduceExtraLetters, "<5>"), (confuseLetters, "<6>"),
+                  (spell.sampleSubstitute, "<7>"), (removePunctuation, "<8>")]
 
 i = 0
 for sentence in sys.stdin:
-  sentence = sentence.strip()
-  functionID = i % len(noiseFunctions)
-  noiseFunction, tag = noiseFunctions[functionID]
-  words = sentence.split()
-  if functionID == 0 or functionID == 1 or functionID == 8:
-    print(noiseFunction(sentence))
-  elif functionID == 7:
-    applicableWords = [(i, w) for i, w in enumerate(words) if w.isalpha() and len(spell.candidates(w)) != 0]
-    if applicableWords == []:
-      print(sentence)
+    sentence = sentence.strip()
+    functionID = i % len(noiseFunctions)
+    noiseFunction, tag = noiseFunctions[functionID]
+    words = sentence.split()
+    if functionID == 0 or functionID == 1 or functionID == 8:
+        print(noiseFunction(sentence))
+    elif functionID == 7:
+        applicableWords = [(i, w) for i, w in enumerate(words) if w.isalpha() and len(spell.candidates(w)) != 0]
+        if applicableWords == []:
+            print(sentence)
+        else:
+            index, w = random.choice(applicableWords)
+            print(" ".join(words[0: index] + [spell.sampleSubstitute(words[index])] + words[index + 1:]))
     else:
-      index, w = random.choice(applicableWords)
-      print(" ".join(words[0 : index] + [spell.sampleSubstitute(words[index])] + words[index+1 : ]))
-  else:
-    applicableWords = [(i, w) for i, w in enumerate(words) if w.isalpha() and len(w) > 2]
-    if applicableWords == []:
-      print(sentence)
-    else:
-      index, w = random.choice(applicableWords)
-      print(" ".join(words[0:index] + [noiseFunction(w)] + words[index+1 : ]))
-  i += 1
+        applicableWords = [(i, w) for i, w in enumerate(words) if w.isalpha() and len(w) > 2]
+        if applicableWords == []:
+            print(sentence)
+        else:
+            index, w = random.choice(applicableWords)
+            print(" ".join(words[0:index] + [noiseFunction(w)] + words[index + 1:]))
+    i += 1
--- src/err2word.py
+++ src/err2word.py
@@ -2,32 +2,35 @@ import random
 import sys
 import spell
 
+
 def checkAplicability(word):
-  try:
-    return any([True for (a,b) in zip(word[:-1], word[1:]) if a.isalpha() and b.isalpha()])
-  except:
-    return False
+    try:
+        return any([True for (a, b) in zip(word[:-1], word[1:]) if a.isalpha() and b.isalpha()])
+    except:
+        return False
 
 
 def applyNoiseFunction(sentence):
-  try:  
-      applicableWords = [(i, w) for i, w in enumerate(sentence.split()) if w.isalpha() and len(spell.candidates(w)) != 0]
-      if applicableWords == []:
-          return sentence.strip()
-      else:
-        lc = sentence.split()
-        index, w = random.choice(applicableWords)
-        #print(" ".join([tag] + lc[0:index] + [noiseFunction(lc[index])] + lc[index+1 : ]))
-        return " ".join(lc[0:index] + [spell.sampleSubstitute(lc[index])] + lc[index+1 : ])
-  except:
-    print(sentence.strip())
+    try:
+        applicableWords = [(i, w) for i, w in enumerate(sentence.split()) if
+                           w.isalpha() and len(spell.candidates(w)) != 0]
+        if applicableWords == []:
+            return sentence.strip()
+        else:
+            lc = sentence.split()
+            index, w = random.choice(applicableWords)
+            # print(" ".join([tag] + lc[0:index] + [noiseFunction(lc[index])] + lc[index+1 : ]))
+            return " ".join(lc[0:index] + [spell.sampleSubstitute(lc[index])] + lc[index + 1:])
+    except:
+        print(sentence.strip())
+
 
 i = 0
 for sentence in sys.stdin:
-  
-  if checkAplicability(sentence):
-      print(i)
-      #applyNoiseFunction(sentence)
-  else:
-    print(i)
-    #print(sentence.strip())
+
+    if checkAplicability(sentence):
+        print(i)
+        # applyNoiseFunction(sentence)
+    else:
+        print(i)
+        # print(sentence.strip())
